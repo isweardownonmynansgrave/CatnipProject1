eb6369c7d38e4b05a26da297a9e6d3f64416056c
isweardownonmynansgrave <lukas.weber@edu.bib.de>
2024-09-12 15:00:01 +0200

    merger PP.cs

--- Assets/PP.cs
+++ Assets/PP.cs
@@ -7,7 +7,7 @@ public class PP : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Kill the Computer at start
+        GameObject pp = new PP();
     }
 
     // Update is called once per frame
